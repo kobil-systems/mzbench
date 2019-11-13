095f1d096463ccd954ba93289961b13f3c0cb6be
Adrian Townsend <adrian.townsend@cognitivesystems.com>
2019-11-13 14:53:11 -0500

    revert to base https for cognitivesystems

--- workers/cognitivesystems/src/cognitivesystems_worker.erl
+++ workers/cognitivesystems/src/cognitivesystems_worker.erl
@@ -2,14 +2,14 @@
 
 -export([initial_state/0, metrics/0]).
 
--export([gatekeeper_connect/4, set_options/3, disconnect/2,
+-export([connect/4, set_options/3, disconnect/2,
     get/3, post/4, put/4, set_prefix/3]).
 
 -type meta() :: [{Key :: atom(), Value :: any()}].
 -type http_options() :: list().
 
 -record(state,
-    { gatekeeper_connection = undefined
+    { connection = undefined
     , prefix = "default"
     , options = [] :: http_options()
     }).
@@ -51,16 +51,16 @@ set_prefix(State, _Meta, NewPrefix) ->
     {nil, State#state{prefix = NewPrefix}}.
 
 -spec disconnect(state(), meta()) -> {nil, state()}.
-disconnect(#state{gatekeeper_connection = Connection} = State, _Meta) ->
+disconnect(#state{connection = Connection} = State, _Meta) ->
     hackney:close(Connection),
     {nil, State}.
 
--spec gatekeeper_connect(state(), meta(), string() | binary(), integer()) -> {nil, state()}.
-gatekeeper_connect(State, Meta, Host, Port) when is_list(Host) ->
-    gatekeeper_connect(State, Meta, list_to_binary(Host), Port);
-gatekeeper_connect(State, _Meta, Host, Port) ->
+-spec connect(state(), meta(), string() | binary(), integer()) -> {nil, state()}.
+connect(State, Meta, Host, Port) when is_list(Host) ->
+    connect(State, Meta, list_to_binary(Host), Port);
+connect(State, _Meta, Host, Port) ->
     {ok, ConnRef} = hackney:connect(hackney_ssl, Host, Port, []),
-    {nil, State#state{gatekeeper_connection = ConnRef}}.
+    {nil, State#state{connection = ConnRef}}.
 
 -spec set_options(state(), meta(), http_options()) -> {nil, state()}.
 set_options(State, _Meta, NewOptions) ->
@@ -69,26 +69,26 @@ set_options(State, _Meta, NewOptions) ->
 -spec get(state(), meta(), string() | binary()) -> {nil, state()}.
 get(State, Meta, Endpoint) when is_list(Endpoint) ->
     get(State, Meta, list_to_binary(Endpoint));
-get(#state{gatekeeper_connection = Connection, prefix = Prefix, options = Options} = State, _Meta, Endpoint) ->
+get(#state{connection = Connection, prefix = Prefix, options = Options} = State, _Meta, Endpoint) ->
     Response = ?TIMED(Prefix ++ ".latency", hackney:send_request(Connection,
         {get, Endpoint, Options, <<>>})),
-    {nil, State#state{gatekeeper_connection = record_response(Prefix, Response)}}.
+    {nil, State#state{connection = record_response(Prefix, Response)}}.
 
 -spec post(state(), meta(), string() | binary(), iodata()) -> {nil, state()}.
 post(State, Meta, Endpoint, Payload) when is_list(Endpoint) ->
     post(State, Meta, list_to_binary(Endpoint), Payload);
-post(#state{gatekeeper_connection = Connection, prefix = Prefix, options = Options} = State, _Meta, Endpoint, Payload) ->
+post(#state{connection = Connection, prefix = Prefix, options = Options} = State, _Meta, Endpoint, Payload) ->
     Response = ?TIMED(Prefix ++ ".latency", hackney:send_request(Connection,
         {post, Endpoint, Options, Payload})),
-    { hackney:body(Connection), State#state{gatekeeper_connection = record_response(Prefix, Response)}}.
+    { hackney:body(Connection), State#state{connection = record_response(Prefix, Response)}}.
 
 -spec put(state(), meta(), string() | binary(), iodata()) -> {nil, state()}.
 put(State, Meta, Endpoint, Payload) when is_list(Endpoint) ->
     put(State, Meta, list_to_binary(Endpoint), Payload);
-put(#state{gatekeeper_connection = Connection, prefix = Prefix, options = Options} = State, _Meta, Endpoint, Payload) ->
+put(#state{connection = Connection, prefix = Prefix, options = Options} = State, _Meta, Endpoint, Payload) ->
     Response = ?TIMED(Prefix ++ ".latency", hackney:send_request(Connection,
         {put, Endpoint, Options, Payload})),
-    {nil, State#state{gatekeeper_connection = record_response(Prefix, Response)}}.
+    {nil, State#state{connection = record_response(Prefix, Response)}}.
 
 record_response(Prefix, Response) ->
     case Response of
