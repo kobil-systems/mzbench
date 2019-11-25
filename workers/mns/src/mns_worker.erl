3ecbc5f8c0328cb746f8931f898be563f4592ebf
Adrian Townsend <adrian.townsend@cognitivesystems.com>
2019-11-25 16:27:58 -0500

    convert to tuples for connect parameters

--- workers/mns/src/mns_worker.erl
+++ workers/mns/src/mns_worker.erl
@@ -274,15 +274,15 @@ record_response(Prefix, Response) ->
 
 -spec mq_cluster_connect(state(), meta() ) -> {nil, state()}.
 mq_cluster_connect(#state{network_mac = FinalMacPrefix, network_id = NetworkId, guardian_id = GuardianId, mq_server = MQServer, mq_password = MQPassword } = State, Meta)->
-    connect(State, Meta, [{"host" , MQServer},
-            {"port" , 1883},
-            {"username" ,  "device"},
-            {"password" , MQPassword},
-            {"client" , fixed_client_id(State, Meta, "pool1", worker_id(State, Meta))},
-            {"clean_session","true"},
-            {"keepalive_interval" , 60},
-            {"proto_version" , 4},
-            {"reconnect_timeout",4}
+    connect(State, Meta, [{host,  MQServer},
+            {port , 1883},
+            {username ,  "device"},
+            {password , MQPassword},
+            {client , fixed_client_id(State, Meta, "pool1", worker_id(State, Meta))},
+            {clean_session,"true"},
+            {keepalive_interval , 60},
+            {proto_version , 4},
+            {reconnect_timeout,4}
             ]),
     {nil, State}.
 
