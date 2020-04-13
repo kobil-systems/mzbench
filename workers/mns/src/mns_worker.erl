9a40d20fcc8a059e437bae47796b9e38f513e3cc
Adrian Townsend <adrian.townsend@cognitivesystems.com>
2020-04-13 17:07:55 -0400

    missing comma

--- workers/mns/src/mns_worker.erl
+++ workers/mns/src/mns_worker.erl
@@ -235,7 +235,7 @@ gk_post(State, Meta, Endpoint, Payload) when is_list(Endpoint) ->
 gk_post(#state{gk_connection = GK_connection, prefix = Prefix, http_options = Options} = State, _Meta, Endpoint, Payload) ->
     Response = ?TIMED(Prefix ++ ".http_latency", hackney:send_request(GK_connection,
         {post, Endpoint, Options, Payload})),
-    {ok, ResponsePayload} =  hackney:body(GK_connection)
+    {ok, ResponsePayload} =  hackney:body(GK_connection),
     RetryCheck = re:run(ResponsePayload, "TRYAGAIN"),
     if
         RetryCheck == nomatch ->
